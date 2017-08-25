b2a5f3b18c015f22ce42743a2bf6fe4b12d4fd81
Guillaume Orlando <30762006+HomardBoy@users.noreply.github.com>
2017-08-25 22:52:58 +0200

    Add files via upload

--- shellcode.sh
+++ shellcode.sh
@@ -1,5 +1,152 @@
 #!/bin/bash
 
+verbose=0
+
+#Création du fichier temporaire d'aide
+    touch help
+    echo "" > help
+    echo "Usage: sh shellcode.sh [-f] [Input] [-o] [Output] [-k] [Output keys] [-v]" >> help
+    echo "       sh shellcode.sh [-h]" >> help
+    echo "" >> help
+    echo "Paramètres:" >> help
+    echo "       [-h] Affiche l'aide." >> help
+    echo "       [-f] Obligatoire. Précède le fichier shellcode à obfusquer." >> help
+    echo "       [-o] Obligatoire. Précède le nom du fichier de sortie du shellcode." >> help
+    echo "       [-k] Obligatoire. Précède le nom du fichier de clés de sortie." >> help
+    echo "       [-v] Optionnel. Mode verbeux. Décrit toutes les actions en cours sur le shellcode et affiche les variables DEBUG" >> help
+    echo "       [Input] Obligatoire. Nom et chemin jusqu'au fichier shellcode à obfusquer." >> help
+    echo "       [Output] Obligatoire. Nom et chemin du fichier shellcode de sortie." >> help
+    echo "       [Output keys] Obligatoire. Nom et chemin du fichier de clés de sortie." >> help
+    echo "" >> help
+    echo "Exemple:" >> help
+    echo "       sh shellcode.sh -f shellcode -o newShellcode -k keys" >> help
+    echo "       sh shellcode.sh -f shellcode -o newShellcode -k keys -v" >> help
+    echo "" >> help
+
+#Test argument null
+if [ $# -eq 0 ]
+  then
+    echo "Paramètres manquants."
+    cat help
+    rm help
+    exit 1
+fi
+
+#Test ordre argument 1 -o
+if [ $1 = "-o" ]
+then
+    echo "Merci de respecter l'ordre des arguments."
+    cat help
+    rm help
+    exit 1
+fi
+
+#Test ordre argument 1 -k
+if [ $1 = "-k" ]
+then
+    echo "Merci de respecter l'ordre des arguments."
+    cat help
+    rm help
+    exit 1
+fi
+
+#Test ordre argument 1 -v
+if [ $1 = "-v" ]
+then
+    echo "Merci de respecter l'ordre des arguments."
+    cat help
+    rm help
+    exit 1
+fi
+
+#Tests argument [-f] [fichier]
+if [ $1 = "-f" ]
+then
+    #Test argument [fichier] existe
+    if [ -z "$2" ]
+    then
+    echo "Paramètres [Input] manquants."
+    cat help
+    rm help
+    exit 1
+    else 
+        #Tests si fichier de l'argument 2 existe
+        if [ -f $2 ]
+        then
+        shellcode=$2
+        else
+        echo "Le fichier d'entrée n'existe pas ou le chemin pour y accéder est erroné."
+        cat help
+        rm help
+        exit 1
+        fi
+    fi
+fi
+
+#Affiche l'aide
+if [ $1 = "-h" ]
+then
+    cat help
+    rm help
+    exit 1
+fi
+
+#Trop d'arguments
+if [ $# -gt 9 ]
+then
+    echo "Trop d'arguments."
+    cat help
+    rm help
+    exit 1
+fi
+
+#Ordre arguments output
+if [ $3 = "-o" ]
+then
+    #Test argument [Output] existe
+    if [ -z $4]
+    then
+        echo "Paramètre [Output] manquant."
+        cat help
+        rm help
+        exit 1
+    else
+    final=$4
+    fi
+else
+echo "Argument [-o] manquant."
+cat help
+rm help
+exit 1
+fi
+
+
+#Tests argument [-k] [Output keys]
+if [ $5 = "-k" ]
+then
+    #Test argument [Output keys] existe
+    if [ -z "$6" ]
+    then
+    echo "Paramètre [Output keys] manquants"
+    cat help
+    rm help
+    exit 1
+    else 
+    finalK=$6
+    fi
+else
+echo "Argument [-k] manquant."
+cat help
+rm help
+exit 1
+fi
+
+#test argument verbosité
+if [ $7 = "-v" ]
+then
+verbose=1
+fi
+
 clear
 okegreen='\033[92m'
 RESET="\033[00m" #normal
@@ -8,7 +155,7 @@ RESET="\033[00m" #normal
 #Retourne le code hexa brute
 # /!\ A OPTIMISER /!\
 touch output
-sed -e 's/"//g' shellcode > output
+sed -e 's/"//g' $shellcode > output
 touch output1
 sed -e 's/;//g' output > output1
 sed -e 's/x//g' output1 > output
@@ -21,12 +168,8 @@ rm output1
 
 #Récupère longueur de la chaine rawHexa + initialisation compteur 
 lenghtRaw=$(wc -c rawHexa | cut -c -3)
-#echo "DEBUG : lenghtRaw = $lenghtRaw"
 lenght=$(($lenghtRaw/2))
-#echo "DEBUG : lenght = $lenght"
 n=0
-
-
 
 #Prépare le fichier qui va contenir le shellcode modifié
 touch finalShellCode
@@ -37,28 +180,26 @@ echo \" > finalShellCode
 #Boucle
 while [ $n != $lenght ]
 do
-#echo "DEBUG : n = $n"
 
 #Avancement
-
-pourcent1=$(($n*100))
-pourcent=$(($pourcent1/$lenght)) 
-clear
-echo $okegreen " "
-echo " • Traitement des données : $pourcent% \r\c"
-echo $RESET " "
+if [ $verbose = "0" ]
+then
+    pourcent1=$(($n*100))
+    pourcent=$(($pourcent1/$lenght)) 
+    clear
+    echo $okegreen " "
+    echo " • Traitement des données : $pourcent% \r\c"
+    echo $RESET " "
+fi
 
 #Récupère deux premiers caractères shellcode brute, soit la première valeure hexa
 hex=$(head -n 1 rawHexa | cut -c -2)
-#echo "DEBUG : hex = $hex"
 
 #Convertion de la valeure hexa en décimal
 deci=$((0x${hex}))
-#echo "DEBUG : decimal = $deci"
 
 #Génère nombre aléatoire entre 1 et 9 avec urandom
 alea=$(grep -m1 -ao '[1-9]' /dev/urandom | sed s/0/9/ | head -n1)
-#echo "DEBUG : aléatoire = $alea"
 
 #Additionne la valeur aléatoire avec le résultat de la conversion héxa
 result=$(($deci + $alea))
@@ -67,7 +208,6 @@ then
     result=$deci
     alea=0
 fi
-#echo "DEBUG : résultat addition en décimal = $result"
 
 #Stock le nombre aléatoire dans un fichier de clés
 touch keys
@@ -83,18 +223,12 @@ rm keys
 
 #Conversion de la nouvelle valeur décimal en héxa + ajout dans le fichier final
 finalHex=$(printf '%x\n' $result)
-#echo "DEBUG : résultat addition en héxa = $finalHex"
 echo "\\x" >> finalShellCode
 echo $finalHex >> finalShellCode
 
 #Suppression des sauts de lignes output
 tr -d '\n' < finalShellCode >> final
 rm finalShellCode
-#echo "DEBUG : résultat final =" && cat final
-#echo ""
-#echo "DEBUG : rawHexa =" && cat rawHexa
-#echo ""
-#echo ""
 
 
 #Supprime 2 premiers caractères de la chaine rawHexa
@@ -103,6 +237,24 @@ echo $supp > rawHexa
 
 #Incrémente compteur
 n=$(($n+1))
+
+#Verbosité activée
+if [ $verbose = "1" ]
+then
+    echo ""
+    echo "DEBUG : lenght = $lenght"
+    echo "DEBUG : lenghtRaw = $lenghtRaw"
+    echo "DEBUG : Boucle n°$n"
+    echo "DEBUG : hex = $hex"
+    echo "DEBUG : decimal = $deci"
+    echo "DEBUG : aléatoire = $alea"
+    echo "DEBUG : résultat addition en décimal = $result"
+    echo "DEBUG : résultat addition en héxa = $finalHex"
+    echo "DEBUG : résultat final =" && cat final
+    echo ""
+    echo "DEBUG : rawHexa =" && cat rawHexa
+    echo ""
+fi
 
 #Fin Boucle
 done
@@ -129,11 +281,16 @@ echo $okegreen " "
 echo "• Fichier modifiée : "
 echo $RESET " "
 cat final
+cp final $final
+rm final
+rm help
 echo ""
 echo $okegreen " "
 echo "• Clés : "
 echo $RESET " "
 cat finalKeys
+cp finalKeys $finalK
+rm finalKeys
 echo ""
 echo ""
 
